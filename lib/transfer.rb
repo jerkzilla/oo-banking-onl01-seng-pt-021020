e91d99e8e0b68b7bac968fd0872faaceb5dd2463
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-10 19:59:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,12 +16,12 @@ class Transfer
     def execute_transaction
       if @sender.balance < @amount
         @status = "rejected"
-        puts "Transaction rejected. Please check your account balance."
+        return "Transaction rejected. Please check your account balance."
       elsif @status = "complete"
         puts "Transaction executed."
       else
         @sender.deposit( @amount * -1)
-        @receiver.deposit( @amount * -1 )
+        @receiver.deposit( @amount  )
         @status = "complete"
       end
     end
