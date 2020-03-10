e3d615804de4e4802fcc881ff8f90263cc45e6f8
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-10 20:12:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status == "complete"
+     @status == "complete"
       @sender.deposit( @amount )
       @receiver.deposit( @amount * -1)
       @status = "reversed"
